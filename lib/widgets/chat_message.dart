83769763a4c80cb2487ff1c694fd2780eba64e74
XxDavexX <dave.santivanez@gmail.com>
2024-07-06 17:45:52 -0500

    avanceDave

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -8,12 +8,15 @@ class ChatMessage extends StatelessWidget {
   ChatMessage({
     required this.message,
     required this.time,
-    required this.userType, 
+    required this.userType,
   });
 
   @override
   Widget build(BuildContext context) {
-    bool isUser1 = userType == 1; 
+    bool isUser1 = userType == 1;
+
+    Color messageTextColor = isUser1 ? Colors.white : Colors.black;
+    Color timeTextColor = isUser1 ? Color.fromARGB(255, 203, 203, 203) : Color.fromARGB(255, 119, 119, 119);
 
     return Column(
       crossAxisAlignment: isUser1 ? CrossAxisAlignment.end : CrossAxisAlignment.start,
@@ -28,6 +31,17 @@ class ChatMessage extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
+          )
+        else
+          Padding(
+            padding: EdgeInsets.only(left: 8.0),
+            child: Text(
+              'Funcy',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         Align(
           alignment: isUser1 ? Alignment.centerRight : Alignment.centerLeft,
@@ -35,12 +49,12 @@ class ChatMessage extends StatelessWidget {
             margin: EdgeInsets.only(
               top: 8.0,
               bottom: 4.0,
-              left: isUser1 ? 80.0 : 4.0, 
+              left: isUser1 ? 80.0 : 4.0,
               right: isUser1 ? 4.0 : 80.0,
             ),
             padding: EdgeInsets.all(10.0),
             decoration: BoxDecoration(
-              color: isUser1 ? Colors.blue[100] : Colors.grey[300],
+              color: isUser1 ? Color(0xFF3D4A7A) : Color(0xFFF2F7FB),
               borderRadius: BorderRadius.circular(10.0),
             ),
             child: Column(
@@ -48,13 +62,13 @@ class ChatMessage extends StatelessWidget {
               children: <Widget>[
                 Text(
                   message,
-                  style: TextStyle(fontSize: 16.0),
+                  style: TextStyle(fontSize: 16.0, color: messageTextColor),
                 ),
                 SizedBox(height: 5.0),
                 Text(
                   time,
-                  textAlign: TextAlign.right, 
-                  style: TextStyle(fontSize: 12.0, color: Colors.grey),
+                  textAlign: TextAlign.right,
+                  style: TextStyle(fontSize: 12.0, color: timeTextColor),
                 ),
               ],
             ),
